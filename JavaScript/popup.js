45e5fadd86dee4d10a2996f5e6045d981130b3d8
vareye <vareye@mcmaster.ca>
2017-01-06 16:07:22 -0500

    Chrome Extension
    This should replace every image with cats!

--- JavaScript/popup.js
+++ JavaScript/popup.js
@@ -1,1 +1,5 @@
 //this is where the javascript application goes
+var imgs = document.getElementsByTagName("img");
+for(var i=0, l=imgs.length; i<l; i++) {
+imgs[i].src = "http://www.rd.com/wp-content/uploads/sites/2/2016/04/01-cat-wants-to-tell-you-laptop.jpg";
+}
