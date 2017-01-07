cc2808eee8c2c7e647a8584eb5cce0aa35a6a37e
vareye <vareye@mcmaster.ca>
2017-01-07 18:21:04 -0500

    Jan 7
    cookie clicker robot to do the clicking for me

--- YearOfCodeEasyJava/src/PlayingWithRobot.java
+++ YearOfCodeEasyJava/src/PlayingWithRobot.java
@@ -1,28 +1,31 @@
+import java.awt.Desktop;
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 
 public class PlayingWithRobot {
-	//i'm going to write a program to organize my files
+	//cookie clicker
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException, URISyntaxException, AWTException, InterruptedException{
+		//Thread.sleep(3000);
+		Robot robot = new Robot();
+		Desktop.getDesktop().browse(new URI("http://orteil.dashnet.org/cookieclicker/"));
+		robot.delay(1000);
+		int x =0;
+		while(x<10000){
+		int mask = InputEvent.BUTTON1_MASK;
+		robot.mouseMove(300, 500);           
+		robot.mousePress(mask);     
+		robot.mouseRelease(mask);
+		System.out.println("click");
+		x++;
+		}
 
-
-	}
-	//takes in directory that will be used
-	public static void getFiles(File f)
-	{
-	File mainFolder = new File("C:\\yourDir");
-	getFiles(mainFolder);
-	   File files[];
-	   if(f.isFile())
-	   System.out.println(f.getAbsolutePath());
-	   else
-	   {
-	      files = f.listFiles();
-	      for (int i = 0; i < files.length; i++) 
-	      {
-	         getFiles(files[i]);
-	      }
-	  }
 	}
 
 }
