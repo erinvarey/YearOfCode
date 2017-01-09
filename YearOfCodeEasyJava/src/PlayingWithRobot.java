b916d33ebab7fbefdbc4892109bae44f9054e504
vareye <vareye@mcmaster.ca>
2017-01-08 21:48:30 -0500

    Added a Timer

--- YearOfCodeEasyJava/src/PlayingWithRobot.java
+++ YearOfCodeEasyJava/src/PlayingWithRobot.java
@@ -3,6 +3,8 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.InputEvent;
@@ -10,22 +12,43 @@ import java.awt.event.KeyEvent;
 
 public class PlayingWithRobot {
 	//cookie clicker
-
+	static Timer timer = new Timer();
+    static int seconds = 0;
+    
 	public static void main(String[] args) throws IOException, URISyntaxException, AWTException, InterruptedException{
 		//Thread.sleep(3000);
-		Robot robot = new Robot();
+		
 		Desktop.getDesktop().browse(new URI("http://orteil.dashnet.org/cookieclicker/"));
-		robot.delay(1000);
-		int x =0;
-		while(x<10000){
-		int mask = InputEvent.BUTTON1_MASK;
-		robot.mouseMove(300, 500);           
-		robot.mousePress(mask);     
-		robot.mouseRelease(mask);
-		System.out.println("click");
-		x++;
-		}
+		MyTimer();
 
 	}
+	public static void MyTimer() {
 
+        TimerTask task;
+
+        task = new TimerTask() {
+            @Override
+            public void run() { 
+				try {
+					Robot robot = new Robot();
+            	robot.delay(10000);
+            	int x =0;
+            	while(x<500000){
+            		int mask = InputEvent.BUTTON1_MASK;
+            		robot.mouseMove(300, 500);           
+            		robot.mousePress(mask);     
+            		robot.mouseRelease(mask);
+            		System.out.println("click");
+            		x++;
+            		}
+				}
+            	catch (AWTException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            }
+        };
+         timer.schedule(task, 0, 50000);
+
+}
 }
