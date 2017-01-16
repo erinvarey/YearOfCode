71bca28a0305217cd00e941f644d7f3008ea5a99
vareye <vareye@mcmaster.ca>
2017-01-16 13:52:25 -0500

    Sombrero cat now dynamically loads
    Sombrero cat application will now dynamically reload the page with cats.
    YAY IMPROVEMENT

--- JavaScript/content.js
+++ JavaScript/content.js
@@ -1,4 +1,9 @@
+//reloads source code every 5 seconds to dynamically load
 var images = document.getElementsByTagName('img');
-for (var i = 0, l = images.length; i < l; i++) {
-  images[i].src = 'http://www.warrenphotographic.co.uk/photography/rubs/19634.jpg' ;
+var Timer = setInterval(myTimer, 5000);
+function myTimer(){
+	var images = document.getElementsByTagName('img');
+	for (var i = 0, l = images.length; i < l; i++) {
+  		images[i].src = 'http://www.warrenphotographic.co.uk/photography/rubs/19634.jpg' ;
+	}
 }
