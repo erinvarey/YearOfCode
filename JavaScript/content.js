bf5e8890a3a2588b357542af378f802040fdea1f
vareye <vareye@mcmaster.ca>
2017-01-15 21:50:51 -0500

    Kitten in Sombraro kinda works now
    It only works for some images though. Going to try again tommorrow

--- JavaScript/content.js
+++ JavaScript/content.js
@@ -1,4 +1,4 @@
 var images = document.getElementsByTagName('img');
 for (var i = 0, l = images.length; i < l; i++) {
-  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+  images[i].src = 'http://www.warrenphotographic.co.uk/photography/rubs/19634.jpg' ;
 }
