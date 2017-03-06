87c913b77d004be2d64983450cc8cbd3f8dc9a79
erinvarey <vareye@mcmaster.ca>
2017-03-06 16:11:16 -0500

    Trying to make a login screen

--- test/app/src/main/java/erin/test/MainActivity.java
+++ test/app/src/main/java/erin/test/MainActivity.java
@@ -4,21 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
     public void onButtonTap(View v){
+        password = (EditText) findViewById(R.id.editText);
         Toast myToast = Toast.makeText(
                 getApplicationContext(),
                 "Ouch!",
                 Toast.LENGTH_LONG);
         myToast.show();
+
 
     }
     //button = (button) findViewById(R.id.button);
@@ -27,8 +30,20 @@ public class MainActivity extends AppCompatActivity {
       //  button.setOnClickListener(new View.OnClickListener() {
         public void onClick(View v) {
             //Start your second activity
-            Intent intent = new Intent(MainActivity.this, secondactivity.class);
-            startActivity(intent);
+            if(password.equals("GIVEMEDOGGO")) {
+                Intent intent = new Intent(MainActivity.this, secondactivity.class);
+                startActivity(intent);
+
+            }
+            else{
+                Toast myToast = Toast.makeText(
+                        getApplicationContext(),
+                        "Please Try again",
+                        Toast.LENGTH_LONG);
+                myToast.show();
+
+            }
+
         }
     }
 
--- test/app/src/main/java/erin/test/secondactivity.java
+++ test/app/src/main/java/erin/test/secondactivity.java
@@ -1,8 +1,16 @@
 package erin.test;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Erin on 05/03/2017.
  */
 
-public class secondactivity {
+public class secondactivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.secondactivity);
+    }
 }
