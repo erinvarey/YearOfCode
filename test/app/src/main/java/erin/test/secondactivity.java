e1e957d924ca64e20e5a303fc37778f430dce681
erinvarey <vareye@mcmaster.ca>
2017-03-08 13:04:06 -0500

    The supidest app in the world continues

--- test/app/src/main/java/erin/test/secondactivity.java
+++ test/app/src/main/java/erin/test/secondactivity.java
@@ -1,7 +1,10 @@
 package erin.test;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by Erin on 05/03/2017.
@@ -13,4 +16,13 @@ public class secondactivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.secondactivity);
     }
+    public void onClick(View v) {
+        Toast myToast = Toast.makeText(
+                getApplicationContext(),
+                "Ouch!",
+                Toast.LENGTH_LONG);
+        myToast.show();
+    }
+       // Intent intent = new Intent(secondactivity.this, thirdactivity.class);
+        //startActivity(intent);
 }
