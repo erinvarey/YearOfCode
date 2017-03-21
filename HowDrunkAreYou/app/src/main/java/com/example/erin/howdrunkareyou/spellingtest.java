05211141c6f2fdd0013cafac5a1d41cdba621c34
erinvarey <vareye@mcmaster.ca>
2017-03-21 12:57:06 -0400

    this didn't auto constrain for some reason

---
+++ HowDrunkAreYou/app/src/main/java/com/example/erin/howdrunkareyou/spellingtest.java
@@ -0,0 +1,17 @@
+package com.example.erin.howdrunkareyou;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Erin on 21/03/2017.
+ */
+
+public class spellingtest extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.spellingtest);
+    }
+}
+
