2beadb5e67e3f2766f45dc1a8ebbff00deaa37fc
erinvarey <vareye@mcmaster.ca>
2017-03-29 12:44:16 -0400

    gotta get this done by the weekend

---
+++ HowDrunkAreYou/app/src/main/java/com/example/erin/howdrunkareyou/TestThreeIntro.java
@@ -0,0 +1,21 @@
+package com.example.erin.howdrunkareyou;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by Erin on 29/03/2017.
+ */
+
+public class TestThreeIntro extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.testthreeintro);
+    }
+    public void onClick(View v) {
+      //  Intent intent = new Intent(TestThreeIntro.this, TestThree.class);
+      //  startActivity(intent);
+    }
+}
--- HowDrunkAreYou/app/src/main/java/com/example/erin/howdrunkareyou/TestTwo.java
+++ HowDrunkAreYou/app/src/main/java/com/example/erin/howdrunkareyou/TestTwo.java
@@ -1,5 +1,6 @@
 package com.example.erin.howdrunkareyou;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,6 +10,8 @@ import android.view.View;
  */
 
 public class TestTwo extends AppCompatActivity {
+    //this has a bug where if you keep hitting the button it jumps up good count
+    //TO BE FIXED ONCE BETA IS DONE
     public Integer goodCount=0;
     public Integer badCount=0;
     Integer grab = MyApplication.getPrecision();
@@ -24,7 +27,9 @@ public class TestTwo extends AppCompatActivity {
         badCount++;
     }
     public void next(View v){
-        Integer sum = goodCount=badCount;
+        Integer sum = goodCount-badCount;
         MyApplication.setPrecision(sum);
+        Intent intent = new Intent(TestTwo.this, TestThreeIntro.class);
+        startActivity(intent);
     }
 }
