6fe6af828eff74995a31bccb912e2692efdd9dbe
erinvarey <vareye@mcmaster.ca>
2017-03-31 22:33:42 -0400

    math now randomized

--- HowDrunkAreYou/app/src/main/java/com/example/erin/howdrunkareyou/TestThree.java
+++ HowDrunkAreYou/app/src/main/java/com/example/erin/howdrunkareyou/TestThree.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 /**
  * Created by Erin on 29/03/2017.
@@ -16,24 +19,87 @@ public class TestThree extends AppCompatActivity {
     private EditText MathAnswerThree;
     Integer grab = MyApplication.getMath();
 
+    private int answer1 = 0;
+    private int answer2 = 0;
+    private int answer3 = 0;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.testthree);
         MathAnswerOne = (EditText) findViewById(R.id.MathAnswerOne);
         MathAnswerTwo = (EditText) findViewById(R.id.MathAnswerTwo);
         MathAnswerThree = (EditText) findViewById(R.id.MathAnswerThree);
+
+        TextView MathOne = (TextView) findViewById(R.id.MathOne);
+        String[] gen = generateMath();
+        String math = gen[0];
+        answer1 = Integer.parseInt(gen[1]);
+        MathOne.setText(math);
+
+        TextView MathTwo = (TextView) findViewById(R.id.MathTwo);
+        gen = generateMath();
+        math = gen[0];
+        answer2 = Integer.parseInt(gen[1]);
+        MathTwo.setText(math);
+
+        TextView MathThree = (TextView) findViewById(R.id.MathThree);
+        gen = generateMath();
+        math = gen[0];
+        answer3 = Integer.parseInt(gen[1]);
+        MathThree.setText(math);
     }
-    public boolean MathRightOne() {
+
+    private String[] generateMath(){
+        Random rand = new Random();
+        String math = "";
+        int nums[] = {rand.nextInt(15), rand.nextInt(15), rand.nextInt(300)};
+        int answer = nums[0];
+        math += nums[0];
+
+        if (rand.nextBoolean()){
+            math += "*";
+            answer *= nums[1];
+        }
+        else{
+            math += "+";
+            answer += nums[1];
+        }
+
+        math += nums[1];
+
+        if (rand.nextBoolean()){
+            math += "+";
+            answer += nums[2];
+        }
+        else{
+            math += "-";
+            answer -= nums[2];
+        }
+
+        math += nums[2];
+
+        String[] output = new String[2];
+        output[0] = math;
+        output[1] = Integer.toString(answer);
+        return output;
+    }
+
+    public int MathRightOne() {
         String a = MathAnswerOne.getText().toString();
         Integer num = 0;
         Integer num2 = 0;
         Integer num3 = 0;
+
+        int correct = 0;
+
         try {
             num = Integer.parseInt(a);
         }catch(NumberFormatException e){
 
         }
-        Boolean answerone= (num == 22);
+        if (num == answer1){
+            correct ++;
+        }
 
         String b=MathAnswerTwo.getText().toString();
         try {
@@ -41,7 +107,9 @@ public class TestThree extends AppCompatActivity {
         }catch(NumberFormatException e){
 
         }
-        Boolean answertwo= (num2==7);
+        if (num2==answer2){
+            correct ++;
+        }
 
         String c=MathAnswerThree.getText().toString();
         try {
@@ -49,20 +117,22 @@ public class TestThree extends AppCompatActivity {
         }catch(NumberFormatException e){
 
         }
-        Boolean answerthree= (num3==47);
+        if (num3==answer3){
+            correct ++;
+        }
 
-        return (answerone && answertwo && answerthree);
+        return correct;
     }
     public void MathDone(View v){
-        if(MathRightOne()){
-            MyApplication.setMath(1);
+       // if(MathRightOne()){
+            MyApplication.setMath(MathRightOne());
+            Intent intent = new Intent(TestThree.this, Results.class);
+            startActivity(intent);
+        //}
+       /* else{
             Intent intent = new Intent(TestThree.this, Results.class);
             startActivity(intent);
         }
-        else{
-            Intent intent = new Intent(TestThree.this, Results.class);
-            startActivity(intent);
-        }
-
+*/
     }
 }
