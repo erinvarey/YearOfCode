91c7122a963d22c4e6c25c19585a142ba11836ae
erinvarey <vareye@mcmaster.ca>
2017-04-11 23:03:13 -0400

    its exams its hard to code now

--- HowDrunkAreYou/app/src/main/java/com/example/erin/howdrunkareyou/TestFour.java
+++ HowDrunkAreYou/app/src/main/java/com/example/erin/howdrunkareyou/TestFour.java
@@ -1,9 +1,25 @@
 package com.example.erin.howdrunkareyou;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Erin on 30/03/2017.
  */
 
-public class TestFour {
+public class TestFour extends AppCompatActivity{
+        protected void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.testthree);
+        }
+    Magicbutton.setOnClickListener(new OnClickListener() {
+
+
+        @Override
+        public void onClick(View v) {
+            Magicbutton button = (Magicbutton) v;
+            button.setVisibility(View.INVISIBLE);
+        }
+    };
 
 }
