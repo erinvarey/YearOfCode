e1ca4817c4e4f3b0e1c08ee48851bfae1c3a1152
erinvarey <vareye@mcmaster.ca>
2017-04-12 23:32:39 -0400

    I should be studying but i coded woops

--- HowDrunkAreYou/app/src/main/java/com/example/erin/howdrunkareyou/TestFour.java
+++ HowDrunkAreYou/app/src/main/java/com/example/erin/howdrunkareyou/TestFour.java
@@ -2,24 +2,31 @@ package com.example.erin.howdrunkareyou;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by Erin on 30/03/2017.
  */
 
 public class TestFour extends AppCompatActivity{
+
+    private Button inputOne;
         protected void onCreate(Bundle savedInstanceState){
             super.onCreate(savedInstanceState);
             setContentView(R.layout.testthree);
-        }
-    Magicbutton.setOnClickListener(new OnClickListener() {
+            inputOne = (Button) findViewById(R.id.Magicbutton);
+
+    inputOne.setOnClickListener(new View.OnClickListener() {
 
 
         @Override
         public void onClick(View v) {
-            Magicbutton button = (Magicbutton) v;
-            button.setVisibility(View.INVISIBLE);
+            //inputOne button = (Magicbutton) v;
+            inputOne.setVisibility(View.INVISIBLE);
         }
-    };
+    }
 
+}
 }
